a1513b661b2b82bce3ca67c9616a859e14121817
anas <130040052+anasrana162@users.noreply.github.com>
2025-02-08 18:12:05 +0500

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -10,14 +10,14 @@ const config = {
   token: '',
   role: '',
   serverKey:
-    'AAAAgpfUtt8:APA91bGmvn82kY1wuyO4wDX89067P81XnQ0XXUvwUR3Jwidh4_uLAMZCMHBzdGm3dgtomBLqXhaLFfHhHhJx_rkH0o-qzbvM942cHGnCnqCHTI0mvtPBWNFLVl5ywLQqEon6F8ZU2c3b',
+    '',
   title: 'ColoniApp',
   fcmtoken: '',
 };
 
 const s3 = new AWS.S3({
-  accessKeyId: 'AKIAQ3EGPNDZLJABVN7C',
-  secretAccessKey: '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',
+  accessKeyId: '',
+  secretAccessKey: '',
   region: 'us-east-1',
 });
 export {config, s3};
